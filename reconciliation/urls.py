eceb18fe7cdb717ec6ee41c9eaa70d44658b2153
lafiagi <skillfullmallam@gmail.com>
2024-10-12 13:31:12 +0100

    Added the task result endpoint

--- reconciliation/urls.py
+++ reconciliation/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
-from reconciliation.views import FileUploadView
+from reconciliation.views import FileUploadView, ReconciliationResultView
 
-urlpatterns = [path("upload/", FileUploadView.as_view(), name="file-upload")]
+urlpatterns = [
+    path("upload/", FileUploadView.as_view(), name="file-upload"),
+    path(
+        "results/<str:task_id>/",
+        ReconciliationResultView.as_view(),
+        name="reconciliation-result",
+    ),
+]
